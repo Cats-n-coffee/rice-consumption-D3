d44331e8f5ca1dbf1d1f325d3217831462fcc948
Cats-n-coffee <lucie.macaigne@gmail.com>
2021-07-24 17:34:33 -0400

    added select year, changed mouse event

--- dist/src.e31bb0bc.js
+++ dist/src.e31bb0bc.js
@@ -34346,7 +34346,13 @@ function _getRequireWildcardCache(nodeInterop) { if (typeof WeakMap !== "functio
 function _interopRequireWildcard(obj, nodeInterop) { if (!nodeInterop && obj && obj.__esModule) { return obj; } if (obj === null || typeof obj !== "object" && typeof obj !== "function") { return { default: obj }; } var cache = _getRequireWildcardCache(nodeInterop); if (cache && cache.has(obj)) { return cache.get(obj); } var newObj = {}; var hasPropertyDescriptor = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var key in obj) { if (key !== "default" && Object.prototype.hasOwnProperty.call(obj, key)) { var desc = hasPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : null; if (desc && (desc.get || desc.set)) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } newObj.default = obj; if (cache) { cache.set(obj, newObj); } return newObj; }
 
 async function draw() {
-  // SVG and container dimensions
+  // Get the selected year
+  d3.select('#years').on('change', function (event) {
+    event.preventDefault();
+    choropleth(this.value);
+  });
+  choropleth("2011"); // SVG and container dimensions
+
   const dimensions = {
     width: 1300,
     height: 1200,
@@ -34355,7 +34361,7 @@ async function draw() {
   dimensions.containerWidth = dimensions.width - dimensions.margin * 2;
   dimensions.containerHeight = dimensions.height - dimensions.margin * 2; // Bind the 2 datasets
 
-  const bindData = async function () {
+  async function bindData() {
     // Fetch the data
     const riceData = await d3.json('rice.json');
     const mapData = await d3.json('https://raw.githubusercontent.com/AshKyd/geojson-regions/master/countries/50m/all.geojson');
@@ -34376,64 +34382,67 @@ async function draw() {
     }
 
     return mapData;
-  }; // Dataset and accessor
+  } // Draw images
 
-
-  const combinedData = await bindData();
-  console.log(combinedData);
-
-  const yearAccessor = d => {
-    return d.properties.data ? d.properties.data["2011"] : null;
-  }; // Create the color scale
-
-
-  const colorSchema = ["#ffefe8", "#ffcfba", "#fcb190", "#ff9a6e", "#fc8956", "#fc7a42", "#fc5d17", "#bf3e06", "#912e03", "#611e01"];
-  const colorScale = d3.scaleQuantize().domain(d3.extent(combinedData.features, yearAccessor)).range(colorSchema); // Create linear scale for legend
-
-  const legendScale = d3.scaleLinear().domain(d3.extent(combinedData.features, yearAccessor)).range([0, 400]); // Map projection
-
-  const mapProjection = d3.geoMercator().fitExtent([[dimensions.margin, dimensions.margin], [dimensions.width - dimensions.margin, dimensions.height - dimensions.margin]], combinedData); // Ensures the GeoJson will cover the available space
-
-  const pathGenerator = d3.geoPath().projection(mapProjection); // Tooltip
-
-  const tooltip = d3.select('#tooltip'); // Draw images
 
   const svg = d3.select('#chart').append('svg').attr('width', dimensions.width).attr('height', dimensions.height);
-  const container = svg.append('g').attr('transform', `translate(${dimensions.margin}, ${dimensions.margin + 15})`);
-  container.selectAll('path').data(combinedData.features).join('path').attr('d', pathGenerator).attr('fill', d => {
-    //console.log('country', d.properties.name_long, 'data', d.properties.data)
-    if (d.properties.data) {
-      if (d.properties.data["2011"] === "...") return "#b3b3b3";else return colorScale(d.properties.data["2011"]);
-    } else {
-      return "#b3b3b3";
-    }
-  }).attr('stroke', 'black').on('mouseenter', function (event, datum) {
-    tooltip.style('display', 'block').style('top', event.layerY + 'px').style('left', event.layerX + 'px');
-    tooltip.select('.tooltip-country span').text(datum.properties.name_long);
-    tooltip.select('.tooltip-quantity span').text(() => {
-      if (datum.properties.data) {
-        if (datum.properties.data["2011"] === "...") return "No data available";else return datum.properties.data["2011"] + ' Kg';
+  const container = svg.append('g').attr('transform', `translate(${dimensions.margin}, ${dimensions.margin + 15})`); // Tooltip
+
+  const tooltip = d3.select('#tooltip'); // ------------------------ Function to create chart
+
+  async function choropleth(selectedYear) {
+    console.log(selectedYear); // One combined dataset
+
+    const combinedData = await bindData();
+    console.log(combinedData); // Accessor
+
+    const yearAccessor = d => {
+      return d.properties.data ? d.properties.data[selectedYear] : null;
+    }; // Create the color scale
+
+
+    const colorSchema = ["#ffefe8", "#ffcfba", "#fcb190", "#ff9a6e", "#fc8956", "#fc7a42", "#fc5d17", "#bf3e06", "#912e03", "#611e01"];
+    const colorScale = d3.scaleQuantize().domain(d3.extent(combinedData.features, yearAccessor)).range(colorSchema); // Map projection
+
+    const mapProjection = d3.geoMercator().fitExtent([[dimensions.margin, dimensions.margin], [dimensions.width - dimensions.margin, dimensions.height - dimensions.margin]], combinedData); // Ensures the GeoJson will cover the available space
+
+    const pathGenerator = d3.geoPath().projection(mapProjection); // Draw the map 
+
+    container.selectAll('path').data(combinedData.features).join('path').attr('d', pathGenerator).attr('fill', d => {
+      //console.log('country', d.properties.name_long, 'data', d.properties.data)
+      if (d.properties.data) {
+        if (d.properties.data[selectedYear] === "...") return "#b3b3b3";else return colorScale(d.properties.data[selectedYear]);
       } else {
-        return "No data available";
+        return "#b3b3b3";
       }
-    }); //console.log(event)
-  }).on('mouseleave', function (event, datum) {
-    tooltip.style('display', 'none');
-  }); // Legend
+    }).attr('stroke', 'black').on('mousemove', function (event, datum) {
+      tooltip.style('display', 'block').style('top', event.layerY + 130 + 'px').style('left', event.layerX + 'px');
+      tooltip.select('.tooltip-country span').text(datum.properties.name_long);
+      tooltip.select('.tooltip-quantity span').text(() => {
+        if (datum.properties.data) {
+          if (datum.properties.data[selectedYear] === "...") return "No data available";else return datum.properties.data[selectedYear] + ' Kg';
+        } else {
+          return "No data available";
+        }
+      }); //console.log(event)
+    }).on('mouseleave', function (event, datum) {
+      tooltip.style('display', 'none');
+    }); // ----- Legend
 
-  const legendGroup = svg.append('g').attr('transform', `translate(${dimensions.width / 4 * 2.5})`); // Grey rectangle and text    
+    const legendGroup = svg.append('g').attr('transform', `translate(${dimensions.width / 4 * 2.5})`); // Legend: Grey rectangle and text    
 
-  const legendNoData = legendGroup.append('g');
-  legendNoData.append('rect').attr('x', 0).attr('y', 0).attr('width', 40).attr('height', 20).attr('fill', '#b3b3b3');
-  legendNoData.append('text').attr('x', 50).attr('y', 15).attr('fill', 'black').text('No data available').style('font-size', '.8rem'); // All colors rectangles and text
+    const legendNoData = legendGroup.append('g');
+    legendNoData.append('rect').attr('x', 0).attr('y', 0).attr('width', 40).attr('height', 20).attr('fill', '#b3b3b3');
+    legendNoData.append('text').attr('x', 50).attr('y', 15).attr('fill', 'black').text('No data available').style('font-size', '.8rem'); // Legend: All colors rectangles and text
 
-  const legendData = legendGroup.append('g');
-  legendData.selectAll('rect').data(colorSchema).join('rect').attr('x', (d, i) => i * 40).attr('y', 30).attr('width', 40).attr('height', 20).attr('fill', d => d);
-  const colorThresholds = colorScale.thresholds().map(d => Math.round(d));
-  colorThresholds.push(colorThresholds[colorThresholds.length - 1] + 30);
-  colorThresholds.unshift(1);
-  console.log(colorThresholds);
-  legendData.selectAll('text').data(colorThresholds).join('text').attr('x', (d, i) => i * 40 - 5).attr('y', 65).attr('fill', 'black').text(d => d).style('font-size', '.8rem');
+    const legendData = legendGroup.append('g');
+    legendData.selectAll('rect').data(colorSchema).join('rect').attr('x', (d, i) => i * 40).attr('y', 30).attr('width', 40).attr('height', 20).attr('fill', d => d); // Legend: get thresholds from colorScale
+
+    const colorThresholds = colorScale.thresholds().map(d => Math.round(d));
+    colorThresholds.push(colorThresholds[colorThresholds.length - 1] + 30);
+    colorThresholds.unshift(1);
+    legendData.selectAll('text').remove().data(colorThresholds).join('text').attr('x', (d, i) => i * 40 - 5).attr('y', 65).attr('fill', 'black').text(d => d).style('font-size', '.8rem');
+  }
 }
 
 draw();
--- src/index.js
+++ src/index.js
@@ -3,6 +3,13 @@ import { color } from 'd3';
 import 'regenerator-runtime/runtime';
 
 async function draw() {
+    // Get the selected year
+    d3.select('#years').on('change', function (event) {
+        event.preventDefault();
+        choropleth(this.value);
+    })
+
+    choropleth("2011")
     // SVG and container dimensions
     const dimensions = {
         width: 1300,
@@ -14,55 +21,25 @@ async function draw() {
     dimensions.containerHeight = dimensions.height - dimensions.margin * 2;
 
     // Bind the 2 datasets
-    const bindData = async function(){
+    async function bindData(){
         // Fetch the data
         const riceData = await d3.json('rice.json');
-       const mapData = await d3.json('https://raw.githubusercontent.com/AshKyd/geojson-regions/master/countries/50m/all.geojson'); 
+        const mapData = await d3.json('https://raw.githubusercontent.com/AshKyd/geojson-regions/master/countries/50m/all.geojson'); 
 
-       for (let j = 0; j < riceData.length; j += 1) {
-           let currentRice = riceData[j];
-           for (let i = 0; i < mapData.features.length; i += 1) {
-                var countryInObj = mapData.features[i].properties;
+        for (let j = 0; j < riceData.length; j += 1) {
+            let currentRice = riceData[j];
+            for (let i = 0; i < mapData.features.length; i += 1) {
+                    var countryInObj = mapData.features[i].properties;
 
-                if (countryInObj.name_long === currentRice.country) {
-                    mapData.features[i].properties = {...mapData.features[i].properties, data: currentRice.data};
-                    break;
-                }
-           }
+                    if (countryInObj.name_long === currentRice.country) {
+                        mapData.features[i].properties = {...mapData.features[i].properties, data: currentRice.data};
+                        break;
+                    }
+            }
         }
-       return mapData;
+        return mapData;
     }
-    
-    // Dataset and accessor
-    const combinedData = await bindData();
-    console.log(combinedData)
-    const yearAccessor = d => {
-        return d.properties.data ? d.properties.data["2011"] : null;
-    };
-
-    // Create the color scale
-    const colorSchema = ["#ffefe8", "#ffcfba", "#fcb190", "#ff9a6e", "#fc8956", "#fc7a42", "#fc5d17", "#bf3e06", "#912e03", "#611e01"];
-    const colorScale = d3.scaleQuantize()
-        .domain(d3.extent(combinedData.features, yearAccessor))
-        .range(colorSchema)
-
-    // Create linear scale for legend
-    const legendScale = d3.scaleLinear()
-        .domain(d3.extent(combinedData.features, yearAccessor))
-        .range([0, 400])
-
-    // Map projection
-    const mapProjection = d3.geoMercator().fitExtent([ 
-            [dimensions.margin, dimensions.margin],
-            [dimensions.width - dimensions.margin, dimensions.height - dimensions.margin]
-        ],
-        combinedData)// Ensures the GeoJson will cover the available space
-
-    const pathGenerator = d3.geoPath().projection(mapProjection)
-
-    // Tooltip
-    const tooltip = d3.select('#tooltip');
-
+   
     // Draw images
     const svg = d3.select('#chart')
         .append('svg')
@@ -71,88 +48,122 @@ async function draw() {
 
     const container = svg.append('g')
         .attr('transform', `translate(${dimensions.margin}, ${dimensions.margin + 15})`)
-    
-    container.selectAll('path')
-        .data(combinedData.features)
-        .join('path')
-        .attr('d', pathGenerator)
-        .attr('fill', d => {
-            //console.log('country', d.properties.name_long, 'data', d.properties.data)
-            if (d.properties.data) {
-                if (d.properties.data["2011"] === "...") return "#b3b3b3";
-                else return colorScale(d.properties.data["2011"]);
-            }
-            else {
-                return "#b3b3b3";
-            } 
-        })
-        .attr('stroke', 'black')
-        .on('mouseenter', function(event, datum){
-            tooltip.style('display', 'block')
-                .style('top', event.layerY + 'px')
-                .style('left', event.layerX + 'px')
 
-            tooltip.select('.tooltip-country span')
-                .text(datum.properties.name_long)
+    // Tooltip
+    const tooltip = d3.select('#tooltip');
 
-            tooltip.select('.tooltip-quantity span')
-                .text(() => {
-                    if (datum.properties.data) {
-                        if (datum.properties.data["2011"] === "...") return "No data available";
-                        else return datum.properties.data["2011"] + ' Kg';
-                    }
-                    else {
-                        return "No data available";
-                    } 
-                })
-//console.log(event)
-        })
-        .on('mouseleave', function(event, datum){
-            tooltip.style('display', 'none');
-        })
 
-    // Legend
-    const legendGroup = svg.append('g')
-        .attr('transform', `translate(${(dimensions.width / 4) * 2.5})`)
+    // ------------------------ Function to create chart
+    async function choropleth(selectedYear) {
+        console.log(selectedYear)
+        
+        // One combined dataset
+    const combinedData = await bindData();
+        console.log(combinedData)
+        // Accessor
+        const yearAccessor = d => {
+            return d.properties.data ? d.properties.data[selectedYear] : null;
+        };
 
-    // Grey rectangle and text    
-    const legendNoData = legendGroup.append('g')
-        legendNoData.append('rect')
-            .attr('x', 0)
-            .attr('y', 0)
-            .attr('width', 40)
-            .attr('height', 20)
-            .attr('fill', '#b3b3b3')
-        legendNoData.append('text')
-            .attr('x', 50)
-            .attr('y', 15)
-            .attr('fill', 'black')
-            .text('No data available')
-            .style('font-size', '.8rem')
-    
-    // All colors rectangles and text
-    const legendData = legendGroup.append('g')
-        legendData.selectAll('rect')
-            .data(colorSchema)
-            .join('rect')
-            .attr('x', (d, i) => i * 40)
-            .attr('y', 30)
-            .attr('width', 40)
-            .attr('height', 20)
-            .attr('fill', d => d)
+        // Create the color scale
+        const colorSchema = ["#ffefe8", "#ffcfba", "#fcb190", "#ff9a6e", "#fc8956", "#fc7a42", "#fc5d17", "#bf3e06", "#912e03", "#611e01"];
+        const colorScale = d3.scaleQuantize()
+            .domain(d3.extent(combinedData.features, yearAccessor))
+            .range(colorSchema)
 
-    const colorThresholds = colorScale.thresholds().map(d => Math.round(d));
-    colorThresholds.push(colorThresholds[colorThresholds.length - 1] + 30)
-    colorThresholds.unshift(1);
-    console.log(colorThresholds)
-        legendData.selectAll('text')
-            .data(colorThresholds)
-            .join('text')
-            .attr('x', (d, i) => i * 40 - 5)
-            .attr('y', 65)
-            .attr('fill', 'black')
-            .text(d => d)
-            .style('font-size', '.8rem')
+        // Map projection
+        const mapProjection = d3.geoMercator().fitExtent([ 
+                [dimensions.margin, dimensions.margin],
+                [dimensions.width - dimensions.margin, dimensions.height - dimensions.margin]
+            ],
+            combinedData)// Ensures the GeoJson will cover the available space
+
+        const pathGenerator = d3.geoPath().projection(mapProjection)
+        
+        // Draw the map 
+        container.selectAll('path')
+            .data(combinedData.features)
+            .join('path')
+            .attr('d', pathGenerator)
+            .attr('fill', d => {
+                //console.log('country', d.properties.name_long, 'data', d.properties.data)
+                if (d.properties.data) {
+                    if (d.properties.data[selectedYear] === "...") return "#b3b3b3";
+                    else return colorScale(d.properties.data[selectedYear]);
+                }
+                else {
+                    return "#b3b3b3";
+                } 
+            })
+            .attr('stroke', 'black')
+            .on('mousemove', function(event, datum){
+                tooltip.style('display', 'block')
+                    .style('top', event.layerY + 130 +'px')
+                    .style('left', event.layerX + 'px')
+
+                tooltip.select('.tooltip-country span')
+                    .text(datum.properties.name_long)
+
+                tooltip.select('.tooltip-quantity span')
+                    .text(() => {
+                        if (datum.properties.data) {
+                            if (datum.properties.data[selectedYear] === "...") return "No data available";
+                            else return datum.properties.data[selectedYear] + ' Kg';
+                        }
+                        else {
+                            return "No data available";
+                        } 
+                    })
+    //console.log(event)
+            })
+            .on('mouseleave', function(event, datum){
+                tooltip.style('display', 'none');
+            })
+
+        // ----- Legend
+        const legendGroup = svg.append('g')
+            .attr('transform', `translate(${(dimensions.width / 4) * 2.5})`)
+
+        // Legend: Grey rectangle and text    
+        const legendNoData = legendGroup.append('g')
+            legendNoData.append('rect')
+                .attr('x', 0)
+                .attr('y', 0)
+                .attr('width', 40)
+                .attr('height', 20)
+                .attr('fill', '#b3b3b3')
+            legendNoData.append('text')
+                .attr('x', 50)
+                .attr('y', 15)
+                .attr('fill', 'black')
+                .text('No data available')
+                .style('font-size', '.8rem')
+        
+        // Legend: All colors rectangles and text
+        const legendData = legendGroup.append('g')
+            legendData.selectAll('rect')
+                .data(colorSchema)
+                .join('rect')
+                .attr('x', (d, i) => i * 40)
+                .attr('y', 30)
+                .attr('width', 40)
+                .attr('height', 20)
+                .attr('fill', d => d)
+
+        // Legend: get thresholds from colorScale
+        const colorThresholds = colorScale.thresholds().map(d => Math.round(d));
+        colorThresholds.push(colorThresholds[colorThresholds.length - 1] + 30)
+        colorThresholds.unshift(1);
+            legendData.selectAll('text')
+            .remove()
+                .data(colorThresholds)
+                .join('text')
+                .attr('x', (d, i) => i * 40 - 5)
+                .attr('y', 65)
+                .attr('fill', 'black')
+                .text(d => d)
+                .style('font-size', '.8rem')
+    }
 }
 
 draw();
