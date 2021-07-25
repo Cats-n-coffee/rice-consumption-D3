b382e167e24f936aaacc92d37eea8a119a42a791
Cats-n-coffee <lucie.macaigne@gmail.com>
2021-07-25 13:35:56 -0400

    overflowX set to scroll

--- dist/src.e31bb0bc.js
+++ dist/src.e31bb0bc.js
@@ -34483,7 +34483,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50460" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50973" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
